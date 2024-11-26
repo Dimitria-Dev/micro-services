4e00c7d5c0970fe42cf82c938ac15decd5aa44b3
Dimitria-Dev <dimitria.dev@gmail.com>
2024-11-26 17:22:26 -0500

    Update AdvisorTests.java

--- advisor/src/test/java/com/microservice/advisor/AdvisorTests.java
+++ advisor/src/test/java/com/microservice/advisor/AdvisorTests.java
@@ -6,6 +6,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+@SpringBootTest
 public class AdvisorTests {
     @Mock
     private AdvisorRepo repo;
